1d4e9f1b93351d6af54c2a53f29b060172816be8
Angel Israel Hernández Testa <123432589+wakeupfilthy@users.noreply.github.com>
2025-06-27 00:39:50 -0600

    page.tsx
    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import Link from 'next/link'; 
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Plane, PenSquare, Compass } from 'lucide-react';
